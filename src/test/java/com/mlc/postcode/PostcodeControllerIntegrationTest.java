d2c3b65a7392a1d39d31c624600394ad9b72726f
Marcelo Lecar <marcelo.lecar@m4u.com.br>
2016-09-21 04:26:23 -0300

    Removing comment

--- src/test/java/com/mlc/postcode/PostcodeControllerIntegrationTest.java
+++ src/test/java/com/mlc/postcode/PostcodeControllerIntegrationTest.java
@@ -1,7 +1,11 @@
 package com.mlc.postcode;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,18 +17,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-
-import com.mlc.postcode.Application;
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(Application.class)
 @WebIntegrationTest
 public class PostcodeControllerIntegrationTest {
-    /*Caching is working in tests*/
 
     @Autowired
     private WebApplicationContext context;
@@ -40,21 +36,13 @@ public class PostcodeControllerIntegrationTest {
 
     @Test
     public void byAddressIreland() throws Exception {
-        mvc.perform(get(baseContext+"/{lookupType}/{country}/{code}", "PCW45-12345-12345-1234X", "address", "ie", "D02X285")
-                .param("lines", "3")
-                .param("format", "json"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$[0].addressline2").value("29-31 Adelaide Road"));
+        mvc.perform(get(baseContext + "/{lookupType}/{country}/{code}", "PCW45-12345-12345-1234X", "address", "ie", "D02X285").param("lines", "3").param("format", "json")).andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON)).andExpect(jsonPath("$[0].addressline2").value("29-31 Adelaide Road"));
     }
-    
+
     @Test
     public void byReverseGeocode() throws Exception {
-        mvc.perform(get(baseContext+"/{lookupType}/{country}/{latitude}/{longitude}", "PCW45-12345-12345-1234X", "rgeo", "ie", "53.332067", "-6.255492")
-                .param("distance", "50")
-                .param("format", "json"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$[0].organisation").value("Irish Pension Trust"));
+        mvc.perform(get(baseContext + "/{lookupType}/{country}/{latitude}/{longitude}", "PCW45-12345-12345-1234X", "rgeo", "ie", "53.332067", "-6.255492").param("distance", "50").param("format", "json")).andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON)).andExpect(jsonPath("$[0].organisation").value("Irish Pension Trust"));
     }
 }
